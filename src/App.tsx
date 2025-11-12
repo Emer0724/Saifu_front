fdee204b34c12a3a9754cc3dff8ee319f890252f
leohu <innocence88226@gamil.com>
2025-11-12 17:47:42 +0800

    燈入頁面雛形

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,24 @@ import ButtonS from './components/button-s';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form action="">
+        <div className="container d-flex flex-column align-items-center justify-content-center vh-100">
+          <h2>Saifu -- Master your money. Master your life.</h2>
+          <div className="box">
+            <label htmlFor="account" className='title'>帳號：</label>
+            <input type="text" id='account' />
+          </div>
+
+          <div className="box">
+            <label htmlFor="pwd" className='title'>密碼：</label>
+            <input type="text"  id='pwd'/>
+          </div>
+          <ButtonS>確認</ButtonS>
+        </div>
+      </form>
     </>
   )
 }
